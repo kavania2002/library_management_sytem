f6f590c6f4f6e34dd02d58a0cd61463b873aaae2
kavania2002 <kavan155gondalia@gmail.com>
2022-04-21 10:41:17 +0530

    Created Librarian and Reader Table

--- initial.sql
+++ initial.sql
@@ -1,1 +1,16 @@
 /*Creating the Librarian Table*/
+CREATE TABLE `lms`.`librarian` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`));
+
+/* Creating Reader's Table */
+CREATE TABLE `lms`.`reader` (
+  `user_id` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NULL,
+  `address` LONGTEXT NULL,
+  `contact_no` INT NULL,
+  PRIMARY KEY (`user_id`));
+
+/*  */
