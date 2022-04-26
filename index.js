6aaac229fd542b70aa332e07017fe347018014a6
kavania2002 <kavan155gondalia@gmail.com>
2022-04-26 23:07:02 +0530

    Add router for Readers

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const readersR = require("./routes/books");
-app.use('/books', readersR);
+const booksR = require("./routes/books");
+app.use('/books', booksR);
 
+const readersR = require("./routes/readers");
+app.use('/readers', readersR);
 
 app.listen(3000, ()=> {
     console.log("Server running at http://localhost:3000/");
