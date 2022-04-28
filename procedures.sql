0e42f0b9546a1674ccedac4c3b07860b04dfda0c
kavania2002 <kavan155gondalia@gmail.com>
2022-04-28 17:46:10 +0530

    Add procedure for searching the book

--- procedures.sql
+++ procedures.sql
@@ -1,6 +1,29 @@
 -- Searching the Book 
+USE `lms`;
+DROP procedure IF EXISTS `search_books_title`;
+
+DELIMITER $$
+USE `lms`$$
+CREATE PROCEDURE `search_books_title` (in book_title varchar(100))
+
+BEGIN
+	declare title varchar(200);
+    set title = concat('%', book_title);
+    set title = concat(title, '%');
+    select title;
+	select * from `book` where `book`.book_id like title;
+END$$
+
+DELIMITER ;
+
+
+
+
 
 -- If book is available or not
+
+
+
 
 -- Automatically add in remove/books via 
 
