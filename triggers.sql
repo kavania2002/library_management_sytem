238342af09bf162bbc396d37546146f69556c0a5
kavania2002 <kavan155gondalia@gmail.com>
2022-04-28 14:01:28 +0530

    Validation of email before insert and update of the table

--- triggers.sql
+++ triggers.sql
@@ -137,9 +137,27 @@ DELIMITER ;
 
 
 -- Email verification
+CREATE DEFINER=`root`@`localhost` TRIGGER `check_email_before` BEFORE INSERT ON `reader` FOR EACH ROW begin
+	declare email_id varchar(100);
+	declare error_msg varchar(100);
+	set email_id = (select new.email from `reader`);
+	if email_id regexp '^[^@]+@[^@]+\.[^@]{2,}$' then
+		set error_msg = `Please enter a valid email address`;
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_msg;
+	end if;	
+end
 
+
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `check_email_update` BEFORE UPDATE ON `reader` FOR EACH ROW begin
+	declare email_id varchar(100);
+	declare error_msg varchar(100);
+	set email_id = (select new.email from `reader`);
+	if email_id regexp '^[^@]+@[^@]+\.[^@]{2,}$' then
+		set error_msg = `Please enter a valid email address`;
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_msg;
+	end if;	
+end
 
 -- Price can't be negative
-
-
 
