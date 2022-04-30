1c33e2b85d337391961f2e33c22683864afe6734
kavania2002 <kavan155gondalia@gmail.com>
2022-04-30 09:00:14 +0530

    Add two apis for books

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,23 @@ router.get("/all", (req, res) => {
   connection.query("select * from books", (error, results, fields) => {
     if (error) throw error;
     const result = resultConvert(results);
-    res.send(resultConvert(results));
+    res.send(result);
+  });
+});
+
+router.get("/:title", (req, res) => {
+  connection.query("call search_book_title(?)", req.params.title, (error, results, fields) => {
+    if (error) console.log(error);
+    const result = resultConvert(results);
+    res.send(result);
+  });
+});
+
+router.get("/available/:title", (req, res) => {
+  connection.query("call check_book_availability(?)", req.params.title, (error, results, fields) => {
+    if (error) console.log(error);
+    const result = resultConvert(results);
+    res.send(result);
   });
 });
 
