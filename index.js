d11c7dbe4ad208d4e17a3b9b02739077e87236c6
kavania2002 <kavan155gondalia@gmail.com>
2022-04-30 09:45:54 +0530

    Add all procedures related to logs

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use('/books', booksR);
 const readersR = require("./routes/readers");
 app.use('/readers', readersR);
 
+const datesR = require("./routes/dates");
+app.use('/dates', datesR);
+
 app.listen(3000, ()=> {
     console.log("Server running at http://localhost:3000/");
 });
